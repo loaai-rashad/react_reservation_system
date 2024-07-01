7c7eccd4b88fade4f39d02eb714dd33dbb3f9294
kofta999 <mostafaxxx555@gmail.com>
2024-07-01 19:26:06 +0300

    fix lots

--- src/Slider.jsx
+++ src/Slider.jsx
@@ -1,36 +1,36 @@
-import {useRef , useEffect , useState} from 'react'
+import { useRef, useEffect, useState } from 'react'
 import { motion } from "framer-motion";
 import images from './images';
 import './Slider.css';
 
-const Slider = () =>  {
-    const carouselref = useRef();
-    const[width , setwidth] = useState(0);
-  
-    useEffect(()=>{
-    setwidth(carouselref.current.scrollWidth - carouselref.current.offsetWidth)
+const Slider = () => {
+   const carouselref = useRef();
+   const [width, setwidth] = useState(0);
 
-  },[]); 
-  return (
-    <div>
-       <motion.div 
-       ref={carouselref} 
-       className="carousel">
-          
-         
-          <motion.div drag="x" 
-          dragConstraints={{right: 0, left: -width }}
-          className="inner-carousel">
-            
-            {images.map((image)=> (
-                  <motion.div className="item" key={image}> 
-                     <img src={image} alt="carousel-img"/>
-                  </motion.div>                       
-                ))}
-          </motion.div>           
-       </motion.div>
-    </div>
-  )
+   useEffect(() => {
+      setwidth(carouselref.current.scrollWidth - carouselref.current.offsetWidth)
+
+   }, []);
+   return (
+      <div>
+         <motion.div
+            ref={carouselref}
+            className="carousel">
+
+
+            <motion.div drag="x"
+               dragConstraints={{ right: 0, left: -width }}
+               className="inner-carousel">
+
+               {images.map((image) => (
+                  <motion.div className="item" key={image}>
+                     <img src={image} alt="carousel-img" />
+                  </motion.div>
+               ))}
+            </motion.div>
+         </motion.div>
+      </div>
+   )
 }
 
 export default Slider
